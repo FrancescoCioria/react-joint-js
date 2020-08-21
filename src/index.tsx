5bc038ebebcf12397eee3674e99f5f59c204c644
Francesco Cioria <firefelix@gmail.com>
2020-08-21 18:37:58 +0200

    add "version" prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
 import * as joint from "jointjs";
-import omit = require("lodash/omit");
 import Portal from "./Portal";
+import omit = require("lodash/omit");
+import { Option, none, some, fold, isNone } from "fp-ts/lib/Option";
+import { identity } from "fp-ts/lib/function";
 
-type Port = joint.dia.Element.Port & {
+export type Port = joint.dia.Element.Port & {
   element: JSX.Element;
   id: string;
   size: { width: number; height: number };
   magnet: "active" | "passive" | "none";
 };
 
-type Node = joint.dia.Element.Attributes & {
+export type Node = joint.dia.Element.Attributes & {
   element: JSX.Element;
   type: string;
   id: string;
@@ -25,6 +27,7 @@ export type Props = {
   nodes: Array<Node>;
   links: Array<joint.dia.Link>;
   paperRef?: (paper: joint.dia.Paper) => void;
+  version?: string;
 };
 
 type State = {
@@ -32,13 +35,14 @@ type State = {
 };
 
 export default class ReactJointJS extends React.Component<Props, State> {
-  paper: joint.dia.Paper = null as any;
+  paper: Option<joint.dia.Paper> = none;
   paperElement: React.RefObject<HTMLDivElement> = React.createRef();
 
   state: State = {
     initialized: false
   };
 
+  // use JSON markup instead of string to remove XML parsing step and improve performance
   foreignObjectMarkup = (
     attributes: {
       width: number;
@@ -46,7 +50,6 @@ export default class ReactJointJS extends React.Component<Props, State> {
       id: string;
     } & { [k: string]: any }
   ): joint.dia.MarkupJSON => [
-    // use JSON markup instead of string to remove XML parsing step and improve perf
     {
       tagName: "foreignObject",
       attributes: {
@@ -56,22 +59,26 @@ export default class ReactJointJS extends React.Component<Props, State> {
   ];
 
   initializeGraph = () => {
-    this.paper = new joint.dia.Paper({
-      // bad typings
-      ...({
-        el: this.paperElement.current,
-        model: this.props.graph
-      } as joint.dia.Paper.Options),
-      ...this.props.paperOptions
-    });
+    const paper = fold<joint.dia.Paper, joint.dia.Paper>(
+      () =>
+        new joint.dia.Paper({
+          // bad typings
+          ...({
+            el: this.paperElement.current,
+            model: this.props.graph
+          } as joint.dia.Paper.Options),
+          ...this.props.paperOptions
+        }),
+      identity
+    )(this.paper);
 
-    this.paper.freeze();
+    paper.freeze();
 
     const cells: joint.dia.Cell[] = [];
 
     this.props.nodes.forEach(node => {
       const nodeElement = new joint.shapes.basic.Generic({
-        ...omit(node, ["element", "ports"]), // JointJS deeply clones everything... remove unnecessary properties to improve perf
+        ...omit(node, ["element", "ports"]), // JointJS deeply clones everything... remove unnecessary properties to improve performance
         markup: this.foreignObjectMarkup({
           width: node.size.width,
           height: node.size.height,
@@ -81,7 +88,7 @@ export default class ReactJointJS extends React.Component<Props, State> {
 
       node.ports.forEach(port =>
         nodeElement.addPort({
-          ...omit(port, ["element", "size", "magnet"]), // JointJS deeply clones everything... remove unnecessary properties to improve perf
+          ...omit(port, ["element", "size", "magnet"]), // JointJS deeply clones everything... remove unnecessary properties to improve performance
           markup: this.foreignObjectMarkup({
             width: port.size.width,
             height: port.size.height,
@@ -101,21 +108,31 @@ export default class ReactJointJS extends React.Component<Props, State> {
     // much better perf than adding each cell with "graph.addCell"
     this.props.graph.resetCells(cells);
 
-    this.props.paperRef && this.props.paperRef(this.paper);
+    if (isNone(this.paper)) {
+      this.paper = some(paper);
+      this.props.paperRef && this.props.paperRef(paper);
 
-    // handles async render
-    (this.paper as any).on("render:done", () => {
-      if (!this.state.initialized) {
-        this.setState({ initialized: true });
-      }
-    });
+      // handles async render
+      (paper as any).on("render:done", () => {
+        if (!this.state.initialized) {
+          this.setState({ initialized: true });
+        }
+      });
+    }
 
     // batch render all cells
-    this.paper.unfreeze();
+    paper.unfreeze();
   };
 
   componentDidMount() {
     this.initializeGraph();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.version !== this.props.version) {
+      this.props.graph.clear();
+      this.setState({ initialized: false }, this.initializeGraph);
+    }
   }
 
   renderNodes = (): JSX.Element[] =>
